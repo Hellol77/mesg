0c4c9b20e7ba46fe2a0488497103a0b98c42a919
DongHyeonWon <dhe7700@naver.com>
2023-11-20 08:23:30 +0900

    rename: 경로 수정

--- frontend/src/hooks/useScoreStore.ts
+++ frontend/src/hooks/useScoreStore.ts
@@ -1,4 +1,4 @@
-import { SURVEY_QUESTION_LENGTH } from "@/constants/Survey";
+import { SURVEY_QUESTION_LENGTH } from "@/constants/survey";
 import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 
